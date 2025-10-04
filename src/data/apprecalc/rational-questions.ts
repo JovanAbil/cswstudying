584ab86e89210591a0fbaef657e8e679a206a3a6
JovanAbil <164279478+JovanAbil@users.noreply.github.com>
2025-10-04 13:45:36 -0400

    Update rational-questions.ts

--- src/data/apprecalc/rational-questions.ts
+++ src/data/apprecalc/rational-questions.ts
@@ -1,3 +1,20 @@
 import { Question } from '@/types/quiz';
 
-export const rationalQuestions: Question[] = [];
+export const rationalQuestions: Question[] = [
+  {
+    "id": "rational-1",
+    "type": "free-response",
+    "question": "Instructions on Image",
+    "image": "/images/apprecalc/rational1.png",
+    "correctAnswer": "As the x-values increase without bound, the y-values approach but never reach -1",
+    "explanation": "The image shows the horizontal asymptote to be -1 which is what will never have a value but the function will try to keep approaching."
+  },
+  {
+    "id": "rational-1",
+    "type": "free-response",
+    "question": "Instructions on Image",
+    "image": "/images/apprecalc/rational2.png",
+    "correctAnswer": "As the x-values increase without bound, the y-values decrease without bound",
+    "explanation": "The image shows the horizontal asymptote to be a negative linear which is treated like a regular function."
+  },
+];
