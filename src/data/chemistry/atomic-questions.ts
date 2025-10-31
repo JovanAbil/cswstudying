42c7b2f733c54af54f64154c400b2e689fcd6f2c
JovanAbil <164279478+JovanAbil@users.noreply.github.com>
2025-10-31 12:42:18 -0400

    Update atomic-questions.ts

--- src/data/chemistry/atomic-questions.ts
+++ src/data/chemistry/atomic-questions.ts
@@ -15,19 +15,6 @@ export const atomicQuestions: Question[] = [
     "explanation": "Democritus first proposed the idea of atoms as tiny, indivisible particles of matter."
   },
   {
-    "id": "atomic-2",
-    "type": "multiple-choice",
-    "question": "Why wasn’t Democritus’s idea of the atom accepted in ancient Greece?",
-    "options": [
-      { "label": "A", "value": "A", "text": "He didn’t publish it" },
-      { "label": "B", "value": "B", "text": "It contradicted the Catholic Church" },
-      { "label": "C", "value": "C", "text": "He had no experimental evidence" },
-      { "label": "D", "value": "D", "text": "It was mathematically incorrect" }
-    ],
-    "correctAnswer": "C",
-    "explanation": "Democritus’s atomic theory was rejected because it lacked experimental proof."
-  },
-  {
     "id": "atomic-3",
     "type": "multiple-choice",
     "question": "Which scientist formulated the first atomic theory based on scientific evidence?",
@@ -251,14 +238,303 @@ export const atomicQuestions: Question[] = [
   {
     "id": "atomic-20",
     "type": "multiple-choice",
-    "question": "Which of these pairs correctly matches the scientist to their experiment?",
+    "image": "/images/chemistry/atomic1.png"
+    "question": "Calculate the average atomic mass of barium, using the following data.",
     "options": [
-      { "label": "A", "value": "A", "text": "Thomson — Gold Foil Experiment" },
-      { "label": "B", "value": "B", "text": "Rutherford — Oil Drop Experiment" },
-      { "label": "C", "value": "C", "text": "Bohr — Flame Test Observations" },
-      { "label": "D", "value": "D", "text": "Chadwick — Cathode Ray Experiment" }
+      { "label": "A", "value": "A", "text": "137.3" },
+      { "label": "B", "value": "B", "text": "137.7" },
+      { "label": "C", "value": "C", "text": "136.6" },
+      { "label": "D", "value": "D", "text": "137.1" }
+    ],
+    "correctAnswer": "D",
+    "explanation": "Math"
+  },
+  {
+    "id": "atomic-21",
+    "type": "multiple-choice",
+    "question": "Which list has elements that are all predicted to have a smaller atonic radius than gallium",
+    "options": [
+      { "label": "A", "value": "A", "text": "Magnesium, Francium, Mercury" },
+      { "label": "B", "value": "B", "text": "Molybdenum, Vanadium, Cadmium" },
+      { "label": "C", "value": "C", "text": "Boron, Arsenic, Bromine" },
+      { "label": "D", "value": "D", "text": "Thallium, Iron, Vanadium" }
+    ],
+    "correctAnswer": "c",
+    "explanation": "Boron, Arsenic, and Bromine are all to the right or above Gallium on the periodic table"
+  },
+  {
+    "id": "atomic-22",
+    "type": "multiple-choice",
+    "question": "Which list has elements that are all predicted to have a higher first ionization energy than tin",
+    "options": [
+      { "label": "A", "value": "A", "text": "Germanium, Phosphorous, Nitrogen" },
+      { "label": "B", "value": "B", "text": "Gold, Oxygen, Selenium" },
+      { "label": "C", "value": "C", "text": "Tantalum, Cesium, Potassium" },
+      { "label": "D", "value": "D", "text": "Flourine, Rubidium, Indium" }
+    ],
+    "correctAnswer": "A",
+    "explanation": "Germanium, Phosphorous, and Nitrogen all are above or more to the right than Tin on the periodic table"
+  },
+  {
+    "id": "atomic-23",
+    "type": "multiple-choice",
+    "question": "Assume that an isotope of curium, Cm-262, and all of its daughter products, are known to radioactively decay using alpha decay. What product results after 4 alpha decays have occured.",
+    "options": [
+      { "label": "A", "value": "A", "text": "U-238" },
+      { "label": "B", "value": "B", "text": "Fr-254" },
+      { "label": "C", "value": "C", "text": "Ra-246" },
+      { "label": "D", "value": "D", "text": "Pa-250" }
     ],
     "correctAnswer": "C",
-    "explanation": "Bohr used flame test and emission spectra observations to support quantized energy levels."
+    "explanation": "262-16 = 246"
+  },
+  {
+    "id": "atomic-24",
+    "type": "multiple-choice",
+    "question": "What list has elements that would be expected to form ions of +1, +2, or +3 charges only.",
+    "options": [
+      { "label": "A", "value": "A", "text": "Sodium, Titanium, Carbon" },
+      { "label": "B", "value": "B", "text": "Silver, Zinc, Gallium" },
+      { "label": "C", "value": "C", "text": "Lithium, Calcium, Nitrogen" },
+      { "label": "D", "value": "D", "text": "Potassium, Tungsten, Oxygen" }
+    ],
+    "correctAnswer": "B",
+    "explanation": "Carbon is 4+ or 4-, Nitrogen and Oxygen form negative bonds so the best option is B"
+  },
+  {
+    "id": "atomic-25",
+    "type": "multiple-choice",
+    "question": "The full electron configuration of a neutral atom is shown below. Which element could it represent? 1s^2 2s^2 2p^6 3s^2 3p^6 4s^2 3s^10 4p^6 5s^2 4d^10 5p^4",
+    "options": [
+      { "label": "A", "value": "A", "text": "Selenium" },
+      { "label": "B", "value": "B", "text": "Tellurium" },
+      { "label": "C", "value": "C", "text": "Tin" },
+      { "label": "D", "value": "D", "text": "Germanium" }
+    ],
+    "correctAnswer": "B",
+    "explanation": "Tellurium is 52 which is 2+2+6+2+6+2+10+6+2+10+4"
+  },
+  {
+    "id": "atomic-26",
+    "type": "multiple-choice",
+    "question": "A photon has a frequency of 6.29 * 10^13 Hz. What is the wavelength of this photon?",
+    "options": [
+      { "label": "A", "value": "A", "text": "4.77 * 10^-6 m" },
+      { "label": "B", "value": "B", "text": "2.10 * 10^5 m" },
+      { "label": "C", "value": "C", "text": "8.61 * 10^-7 m" },
+      { "label": "D", "value": "D", "text": "3.55 * 10^4 m" }
+    ],
+    "correctAnswer": "A",
+    "explanation": "Math"
+  },  
+  { //IMAGE
+    "id": "atomic-27",
+    "type": "multiple-choice",
+    "question": "Which of the following is the orbitl diagram for chromium",
+    "options": [
+      { "label": "A", "value": "A", "text": "", "image": "/images/apprecalc/atomic2.png"},
+      { "label": "B", "value": "B", "text": "", "image": "/images/apprecalc/atomic3.png" },
+      { "label": "C", "value": "C", "text": "", "image": "/images/apprecalc/atomic4.png" },
+      { "label": "D", "value": "D", "text": "", "image": "/images/apprecalc/atomic5.png" }
+    ],
+    "correctAnswer": "C",
+    "explanation": "Correct amount of electrons and correct format"
+  },
+  {
+    "id": "atomic-28",
+    "type": "multiple-choice",
+    "question": "Identify how many subatomic particles this particular isotope has  232/92 U^(4+)",
+    "options": [
+      { "label": "A", "value": "A", "text": "Protons = 88 Neutrons = 140 Electrons = 92" },
+      { "label": "B", "value": "B", "text": "Protons = 140 Neutrons = 232 Electrons = 96" },
+      { "label": "C", "value": "C", "text": "Protons = 96 Neutrons = 136 Electrons = 88" },
+      { "label": "D", "value": "D", "text": "Protons = 92 Neutrons = 140 Electrons = 88" }
+    ],
+    "correctAnswer": "D",
+    "explanation": "Only one with the correct amount of protons"
+  },
+  {
+    "id": "atomic-29",
+    "type": "multiple-choice",
+    "question": "Which nuclear process does the following statement describe? 'Two nuclei collide and form a larger nucleus and release energy'",
+    "options": [
+      { "label": "A", "value": "A", "text": "Fission" },
+      { "label": "B", "value": "B", "text": "Gamma Radiation" },
+      { "label": "C", "value": "C", "text": "Alpha Decay" },
+      { "label": "D", "value": "D", "text": "Fusion" }
+    ],
+    "correctAnswer": "D",
+    "explanation": "Fusion is when 2 nuclei collide and combine to form a larger element and releases energy"
+  },
+  {
+    "id": "atomic-30",
+    "type": "multiple-choice",
+    "question": "A sample of a radioactive isotope has a half-life of 15 minutes. If the sample is originally 400 g, how many grams of the daughter product have been formed after 45 minutes?",
+    "options": [
+      { "label": "A", "value": "A", "text": "50 g" },
+      { "label": "B", "value": "B", "text": "350 g" },
+      { "label": "C", "value": "C", "text": "12.5 g" },
+      { "label": "D", "value": "D", "text": "There is not enough information to determine this" }
+    ],
+    "correctAnswer": "B",
+    "explanation": "It is asking for daughter product, 45/15 = 3 and after 3 half lifes 50 grams remain which means the other 350 have been turned into the daughter product."
+  },
+  {
+    "id": "atomic-31",
+    "type": "multiple-choice",
+    "question": "Which list of elements contains a metal, nonmetal, and metalloid?",
+    "options": [
+      { "label": "A", "value": "A", "text": "Sc, As, H" },
+      { "label": "B", "value": "B", "text": "Rb, Al, F" },
+      { "label": "C", "value": "C", "text": "Ca, Cl, He" },
+      { "label": "D", "value": "D", "text": "Hg, Br, U" }
+    ],
+    "correctAnswer": "A",
+    "explanation": "Only one that fits the requirements."
+  },
+  {
+    "id": "atomic-32",
+    "type": "multiple-choice",
+    "question": "Select the true statement regarding nuclear decay particles",
+    "options": [
+      { "label": "A", "value": "A", "text": "Gamma particles have no mass" },
+      { "label": "B", "value": "B", "text": "A parent isotope gains protons during alpha decay" },
+      { "label": "C", "value": "C", "text": "Beta particles can penetrate lead blocks" },
+      { "label": "D", "value": "D", "text": "Alpha particles have no charge" }
+    ],
+    "correctAnswer": "A",
+    "explanation": "Gamma particles don't have mass; best answer."
+  },
+  {
+    "id": "atomic-33",
+    "type": "multiple-choice",
+    "question": "Polonium-204 is unstable, and undergoes decay by emitting an electron. This is an example of _____ decay.",
+    "options": [
+      { "label": "A", "value": "A", "text": "Alpha" },
+      { "label": "B", "value": "B", "text": "Beta" },
+      { "label": "C", "value": "C", "text": "Gamma" },
+      { "label": "D", "value": "D", "text": "Cannot be determined" }
+    ],
+    "correctAnswer": "B",
+    "explanation": "Beta decay emits a high energy electron/positron and then the protons increase(if emitted an electron)/decrease(if emitted a positron) by 1"
+  },
+  {
+    "id": "atomic-34",
+    "type": "multiple-choice",
+    "question": "Which of these is the identity of a Polonium-204 that underwent a decay by emitting an electron?",
+    "options": [
+      { "label": "A", "value": "A", "text": "204/84 Po" },
+      { "label": "B", "value": "B", "text": "204/85 At" },
+      { "label": "C", "value": "C", "text": "203/83 Bi" },
+      { "label": "D", "value": "D", "text": "205/83 Rn" }
+    ],
+    "correctAnswer": "B",
+    "explanation": "It goes under beta decay so it the atomic number increased by 1"
+  },
+  {
+    "id": "atomic-35",
+    "type": "multiple-choice",
+    "question": "Calculate the energy of a photon with a wavelength of 3.15 * 10^-5 m",
+    "options": [
+      { "label": "A", "value": "A", "text": "8.57 * 10^12 J" },
+      { "label": "B", "value": "B", "text": "1.89 * 10^-29 J" },
+      { "label": "C", "value": "C", "text": "6.31 * 10^-21 J" },
+      { "label": "D", "value": "D", "text": "1.76 * 10^20 J" }
+    ],
+    "correctAnswer": "C",
+    "explanation": "Math"
+  },
+  {
+    "id": "atomic-36",
+    "type": "multiple-choice",
+    "question": "Select the true statement regarding light waves",
+    "options": [
+      { "label": "A", "value": "A", "text": "Ultraviolet light has a larger wavelength than microwaves" },
+      { "label": "B", "value": "B", "text": "Infrared light has a larger wavelength than x-rays" },
+      { "label": "C", "value": "C", "text": "Radio wave photons have more energy than gamma ray photons" },
+      { "label": "D", "value": "D", "text": "Orange light has a higher freqency than green light" }
+    ],
+    "correctAnswer": "B",
+    "explanation": "Infrared light has a larger wavelength than x-rays because it has less energy in the photons"
+  },
+  {
+    "id": "atomic-37",
+    "type": "multiple-choice",
+    "question": "Approximately how long would it take for 75% of a 20g sample of Am-243 to decay?",
+    "image": "/images/chemistry/atomic6.png"
+    "options": [
+      { "label": "A", "value": "A", "text": "3500 years" },
+      { "label": "B", "value": "B", "text": "7500 years" },
+      { "label": "C", "value": "C", "text": "15000 years" },
+      { "label": "D", "value": "D", "text": "22500 years" }
+    ],
+    "correctAnswer": "C",
+    "explanation": "Math"
+  },
+  {
+    "id": "atomic-38",
+    "type": "multiple-choice",
+    "question": "What is the noble gas configuration of molybdenum",
+    "options": [
+      { "label": "A", "value": "A", "text": "[Ar] 4s^2 4p^6 5s^2 4d^4" },
+      { "label": "B", "value": "B", "text": "[Ar] 4s^2 3d^4" },
+      { "label": "C", "value": "C", "text": "[Kr] 5s^2 4d^4" },
+      { "label": "D", "value": "D", "text": "[Kr] 5s^2" }
+    ],
+    "correctAnswer": "C",
+    "explanation": "Counting the electrons; 36+2+4=42 which is molybdenum"
+  },
+  {
+    "id": "atomic-39",
+    "type": "multiple-choice",
+    "question": "Which of these is the correct electron configuration for Se^(2-)",
+    "options": [
+      { "label": "A", "value": "A", "text": "1s^2 2s^2 2p^6 3s^2 3p^6 4s^2 3d^10 4p^2" },
+      { "label": "B", "value": "B", "text": "1s^2 2s^2 2p^6 3s^2 3p^6 4s^2 3d^10 4p^4" },
+      { "label": "C", "value": "C", "text": "1s^2 2s^2 2p^6 3s^2 3p^6 4s^2 3d^10 4p^6" },
+      { "label": "D", "value": "D", "text": "1s^2 2s^2 2p^6 3s^2 3p^6 4s^2 3d^10 4p^6 5s^2" }
+    ],
+    "correctAnswer": "C",
+    "explanation": "1s^2 2s^2 2p^6 3s^2 3p^6 4s^2 3d^10 4p^6 has the configuration of 36 electrons which is Se^(2-)"
+  },
+  {
+    "id": "atomic-40",
+    "type": "multiple-choice",
+    "question": "Select the incorrect statement about valence electrons",
+    "options": [
+      { "label": "A", "value": "A", "text": "Noble gases have full valence electron shells" },
+      { "label": "B", "value": "B", "text": "Valence electrons are on the outermost shell" },
+      { "label": "C", "value": "C", "text": "Calcium's valence electrons are in the n = 3 shell" },
+    ],
+    "correctAnswer": "C",
+    "explanation": "Valence electrons are on the outermost shell and Noble gases have full valence electrons which makes the other statement false by default."
+  },
+  {
+    "id": "atomic-41",
+    "type": "multiple-choice",
+    "question": "Use the table to predict the atomic radius of chlorine",
+    "image": "/images/chemistry/atomic7.png"
+    "options": [
+      { "label": "A", "value": "A", "text": "101 pm" },
+      { "label": "B", "value": "B", "text": "174 pm" },
+      { "label": "C", "value": "C", "text": "210 pm" },
+      { "label": "D", "value": "D", "text": "283 pm" }
+    ],
+    "correctAnswer": "B",
+    "explanation": "The pm should be above 133 or below 196 and only 174 pm follows that rule."
+  },
+  {
+    "id": "atomic-42",
+    "type": "free-response",
+    "question": "Costanzium has two isotopes: Cz-329 (329.0308 amu) and Cz-332(331.9935 amu). If the average atomic mass of Costanzium is 330.312 amu, determine the percent abundances of BOTH isotopes.",
+    "correctAnswer": "Cz has an abundance of 56.27% while Cz-332 has an abundance of 43.78%",
+    "explanation": "329.0308(x)+331.9935(1-x)=330.312; x ~ 0.5627"
+  },
+  {
+    "id": "atomic-43",
+    "type": "free-response",
+    "question": "",
+    "correctAnswer": "",
+    "explanation": ""
   },
 ];
