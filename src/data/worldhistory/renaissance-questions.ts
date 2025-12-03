eec6e74eed518dfaeab84689a86312a0361e91d7
JovanAbil <164279478+JovanAbil@users.noreply.github.com>
2025-12-02 21:39:16 -0500

    Update renaissance-questions.ts

--- src/data/worldhistory/renaissance-questions.ts
+++ src/data/worldhistory/renaissance-questions.ts
@@ -1,11 +1,352 @@
 import { Question } from '@/types/quiz';
 
 export const renaissanceQuestions: Question[] = [
-  
   {
     id: 'renaissance-',
     type: 'free-response',
     question: '',
+    correctAnswer: '',
+    explanation: ''
+  },
+  {
+    id: 'renaissance-',
+    type: 'free-response',
+    question: '',
+    correctAnswer: '',
+    explanation: ''
+  },
+  {
+    id: 'renaissance-',
+    type: 'free-response',
+    question: '',
+    correctAnswer: '',
+    explanation: ''
+  },
+  {
+    id: 'renaissance-',
+    type: 'free-response',
+    question: '',
+    correctAnswer: '',
+    explanation: ''
+  },
+  {
+    id: 'renaissance-',
+    type: 'free-response',
+    question: '',
+    correctAnswer: '',
+    explanation: ''
+  },
+  {
+    id: 'renaissance-',
+    type: 'free-response',
+    question: '',
+    correctAnswer: '',
+    explanation: ''
+  },
+  {
+    id: 'renaissance-',
+    type: 'free-response',
+    question: '',
+    correctAnswer: '',
+    explanation: ''
+  },
+  {
+    id: 'renaissance-',
+    type: 'free-response',
+    question: '',
+    correctAnswer: '',
+    explanation: ''
+  },
+  {
+    id: 'renaissance-',
+    type: 'free-response',
+    question: '',
+    correctAnswer: '',
+    explanation: ''
+  },
+  {
+    id: 'renaissance-',
+    type: 'free-response',
+    question: '',
+    correctAnswer: '',
+    explanation: ''
+  },
+  {
+    id: 'renaissance-',
+    type: 'free-response',
+    question: '',
+    correctAnswer: '',
+    explanation: ''
+  },
+  {
+    id: 'renaissance-',
+    type: 'free-response',
+    question: '',
+    correctAnswer: '',
+    explanation: ''
+  },
+  {
+    id: 'renaissance-',
+    type: 'free-response',
+    question: '',
+    correctAnswer: '',
+    explanation: ''
+  },
+  {
+    id: 'renaissance-',
+    type: 'free-response',
+    question: '',
+    correctAnswer: '',
+    explanation: ''
+  },
+  {
+    id: 'renaissance-',
+    type: 'free-response',
+    question: '',
+    correctAnswer: '',
+    explanation: ''
+  },
+  {
+    id: 'renaissance-',
+    type: 'free-response',
+    question: '',
+    correctAnswer: '',
+    explanation: ''
+  },
+  {
+    id: 'renaissance-',
+    type: 'free-response',
+    question: '',
+    correctAnswer: '',
+    explanation: ''
+  },
+  {
+    id: 'renaissance-',
+    type: 'free-response',
+    question: '',
+    correctAnswer: '',
+    explanation: ''
+  },
+  {
+    id: 'renaissance-',
+    type: 'free-response',
+    question: '',
+    correctAnswer: '',
+    explanation: ''
+  },
+  {
+    id: 'renaissance-',
+    type: 'free-response',
+    question: '',
+    correctAnswer: '',
+    explanation: ''
+  },
+  {
+    id: 'renaissance-',
+    type: 'multiple-choice',
+    question: '',
+    options: [
+      { label: 'A', value: 'A', text: 'True' },
+      { label: 'B', value: 'B', text: 'False' },
+    ],
+    correctAnswer: '',
+    explanation: ''
+  },
+  {
+    id: 'renaissance-',
+    type: 'multiple-choice',
+    question: '',
+    options: [
+      { label: 'A', value: 'A', text: 'True' },
+      { label: 'B', value: 'B', text: 'False' },
+    ],
+    correctAnswer: '',
+    explanation: ''
+  },
+  {
+    id: 'renaissance-',
+    type: 'multiple-choice',
+    question: '',
+    options: [
+      { label: 'A', value: 'A', text: 'True' },
+      { label: 'B', value: 'B', text: 'False' },
+    ],
+    correctAnswer: '',
+    explanation: ''
+  },
+  {
+    id: 'renaissance-',
+    type: 'multiple-choice',
+    question: '',
+    options: [
+      { label: 'A', value: 'A', text: 'True' },
+      { label: 'B', value: 'B', text: 'False' },
+    ],
+    correctAnswer: '',
+    explanation: ''
+  },
+  {
+    id: 'renaissance-',
+    type: 'multiple-choice',
+    question: '',
+    options: [
+      { label: 'A', value: 'A', text: 'True' },
+      { label: 'B', value: 'B', text: 'False' },
+    ],
+    correctAnswer: '',
+    explanation: ''
+  },
+  {
+    id: 'renaissance-',
+    type: 'multiple-choice',
+    question: '',
+    options: [
+      { label: 'A', value: 'A', text: 'True' },
+      { label: 'B', value: 'B', text: 'False' },
+    ],
+    correctAnswer: '',
+    explanation: ''
+  },
+  {
+    id: 'renaissance-',
+    type: 'multiple-choice',
+    question: '',
+    options: [
+      { label: 'A', value: 'A', text: 'True' },
+      { label: 'B', value: 'B', text: 'False' },
+    ],
+    correctAnswer: '',
+    explanation: ''
+  },
+  {
+    id: 'renaissance-',
+    type: 'multiple-choice',
+    question: '',
+    options: [
+      { label: 'A', value: 'A', text: 'True' },
+      { label: 'B', value: 'B', text: 'False' },
+    ],
+    correctAnswer: '',
+    explanation: ''
+  },
+  {
+    id: 'renaissance-',
+    type: 'multiple-choice',
+    question: '',
+    options: [
+      { label: 'A', value: 'A', text: 'True' },
+      { label: 'B', value: 'B', text: 'False' },
+    ],
+    correctAnswer: '',
+    explanation: ''
+  },
+  {
+    id: 'renaissance-',
+    type: 'multiple-choice',
+    question: '',
+    options: [
+      { label: 'A', value: 'A', text: 'True' },
+      { label: 'B', value: 'B', text: 'False' },
+    ],
+    correctAnswer: '',
+    explanation: ''
+  },
+  {
+    id: 'renaissance-',
+    type: 'multiple-choice',
+    question: '',
+    options: [
+      { label: 'A', value: 'A', text: 'True' },
+      { label: 'B', value: 'B', text: 'False' },
+    ],
+    correctAnswer: '',
+    explanation: ''
+  },
+  {
+    id: 'renaissance-',
+    type: 'multiple-choice',
+    question: '',
+    options: [
+      { label: 'A', value: 'A', text: 'True' },
+      { label: 'B', value: 'B', text: 'False' },
+    ],
+    correctAnswer: '',
+    explanation: ''
+  },
+  {
+    id: 'renaissance-',
+    type: 'multiple-choice',
+    question: '',
+    options: [
+      { label: 'A', value: 'A', text: 'True' },
+      { label: 'B', value: 'B', text: 'False' },
+    ],
+    correctAnswer: '',
+    explanation: ''
+  },
+  {
+    id: 'renaissance-',
+    type: 'multiple-choice',
+    question: '',
+    options: [
+      { label: 'A', value: 'A', text: 'True' },
+      { label: 'B', value: 'B', text: 'False' },
+    ],
+    correctAnswer: '',
+    explanation: ''
+  },
+  {
+    id: 'renaissance-',
+    type: 'multiple-choice',
+    question: '',
+    options: [
+      { label: 'A', value: 'A', text: 'True' },
+      { label: 'B', value: 'B', text: 'False' },
+    ],
+    correctAnswer: '',
+    explanation: ''
+  },
+  {
+    id: 'renaissance-',
+    type: 'multiple-choice',
+    question: '',
+    options: [
+      { label: 'A', value: 'A', text: 'True' },
+      { label: 'B', value: 'B', text: 'False' },
+    ],
+    correctAnswer: '',
+    explanation: ''
+  },
+  {
+    id: 'renaissance-',
+    type: 'multiple-choice',
+    question: '',
+    options: [
+      { label: 'A', value: 'A', text: 'True' },
+      { label: 'B', value: 'B', text: 'False' },
+    ],
+    correctAnswer: '',
+    explanation: ''
+  },
+  {
+    id: 'renaissance-',
+    type: 'multiple-choice',
+    question: '',
+    options: [
+      { label: 'A', value: 'A', text: 'True' },
+      { label: 'B', value: 'B', text: 'False' },
+    ],
+    correctAnswer: '',
+    explanation: ''
+  },
+  {
+    id: 'renaissance-',
+    type: 'multiple-choice',
+    question: '',
+    options: [
+      { label: 'A', value: 'A', text: 'True' },
+      { label: 'B', value: 'B', text: 'False' },
+    ],
     correctAnswer: '',
     explanation: ''
   },
@@ -30,6 +371,137 @@ export const renaissanceQuestions: Question[] = [
       { label: 'C', value: 'C', text: '' },
       { label: 'D', value: 'D', text: '' },
     ],
+    correctAnswer: '',
+    explanation: ''
+  },
+  {
+    id: 'renaissance-',
+    type: 'multiple-choice',
+    question: '',
+    options: [
+      { label: 'A', value: 'A', text: '' },
+      { label: 'B', value: 'B', text: '' },
+      { label: 'C', value: 'C', text: '' },
+      { label: 'D', value: 'D', text: '' },
+    ],
+    correctAnswer: '',
+    explanation: ''
+  },
+  {
+    id: 'renaissance-',
+    type: 'multiple-choice',
+    question: '',
+    options: [
+      { label: 'A', value: 'A', text: '' },
+      { label: 'B', value: 'B', text: '' },
+      { label: 'C', value: 'C', text: '' },
+      { label: 'D', value: 'D', text: '' },
+    ],
+    correctAnswer: '',
+    explanation: ''
+  },
+  {
+    id: 'renaissance-',
+    type: 'multiple-choice',
+    question: '',
+    options: [
+      { label: 'A', value: 'A', text: '' },
+      { label: 'B', value: 'B', text: '' },
+      { label: 'C', value: 'C', text: '' },
+      { label: 'D', value: 'D', text: '' },
+    ],
+    correctAnswer: '',
+    explanation: ''
+  },
+  {
+    id: 'renaissance-',
+    type: 'multiple-choice',
+    question: '',
+    options: [
+      { label: 'A', value: 'A', text: '' },
+      { label: 'B', value: 'B', text: '' },
+      { label: 'C', value: 'C', text: '' },
+      { label: 'D', value: 'D', text: '' },
+    ],
+    correctAnswer: '',
+    explanation: ''
+  },
+  {
+    id: 'renaissance-',
+    type: 'multiple-choice',
+    question: '',
+    options: [
+      { label: 'A', value: 'A', text: '' },
+      { label: 'B', value: 'B', text: '' },
+      { label: 'C', value: 'C', text: '' },
+      { label: 'D', value: 'D', text: '' },
+    ],
+    correctAnswer: '',
+    explanation: ''
+  },
+  {
+    id: 'renaissance-',
+    type: 'multiple-choice',
+    question: '',
+    options: [
+      { label: 'A', value: 'A', text: '' },
+      { label: 'B', value: 'B', text: '' },
+      { label: 'C', value: 'C', text: '' },
+      { label: 'D', value: 'D', text: '' },
+    ],
+    correctAnswer: '',
+    explanation: ''
+  },
+  {
+    id: 'renaissance-',
+    type: 'multiple-choice',
+    question: '',
+    options: [
+      { label: 'A', value: 'A', text: '' },
+      { label: 'B', value: 'B', text: '' },
+      { label: 'C', value: 'C', text: '' },
+      { label: 'D', value: 'D', text: '' },
+    ],
+    correctAnswer: '',
+    explanation: ''
+  },
+  {
+    id: 'renaissance-',
+    type: 'multiple-choice',
+    question: '',
+    options: [
+      { label: 'A', value: 'A', text: '' },
+      { label: 'B', value: 'B', text: '' },
+      { label: 'C', value: 'C', text: '' },
+      { label: 'D', value: 'D', text: '' },
+    ],
+    correctAnswer: '',
+    explanation: ''
+  },
+  {
+    id: 'renaissance-',
+    type: 'multiple-choice',
+    question: '',
+    options: [
+      { label: 'A', value: 'A', text: '' },
+      { label: 'B', value: 'B', text: '' },
+      { label: 'C', value: 'C', text: '' },
+      { label: 'D', value: 'D', text: '' },
+    ],
+    correctAnswer: '',
+    explanation: ''
+  },
+  {
+    id: 'renaissance-',
+    type: 'free-response',
+    question: '',
+    correctAnswer: '',
+    explanation: ''
+  },
+  {
+    id: 'renaissance-',
+    type: 'free-response',
+    question: '',
     correctAnswer: '',
     explanation: ''
   },
